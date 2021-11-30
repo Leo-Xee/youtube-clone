c232cff5bec4b2e28d9b7034f494b6dc03dff937
Leo-Xee <jang.xmin@gmail.com>
2021-12-01 01:34:21 +0900

    [ADD] mongoose 추가

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+const { mongoURI } = require('./config/key.js');
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log('MongoDB Connected!!'))
+  .catch((e) => console.log(e));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
