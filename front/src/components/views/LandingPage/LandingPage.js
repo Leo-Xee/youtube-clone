cb9d285bc4f0d7334f04edf631d5b9a9502400d8
Leo-Xee <jang.xmin@gmail.com>
2021-12-05 11:41:53 +0900

    [ADD] Proxy로 CORS 문제 해결

--- front/src/components/views/LandingPage/LandingPage.js
+++ front/src/components/views/LandingPage/LandingPage.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export default function LandingPage() {
   useEffect(() => {
-    axios.get('http://localhost:5000/').then((res) => console.log(res));
+    axios.get('/').then((res) => console.log(res));
   }, []);
   return <div>LandingPage</div>;
 }
