442637ea76137a2100f3ed84755676b6fa4de51f
Leo-Xee <jang.xmin@gmail.com>
2021-12-05 11:34:38 +0900

    [FIX] App의 import 오류수정

--- front/src/components/views/LandingPage/LandingPage.js
+++ front/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 export default function LandingPage() {
+  useEffect(() => {
+    axios.get('http://localhost:5000/').then((res) => console.log(res));
+  }, []);
   return <div>LandingPage</div>;
 }
